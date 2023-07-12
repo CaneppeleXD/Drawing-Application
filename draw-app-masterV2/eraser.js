abc8b6e0e37bbea0c0e300958610a46f67c8dce1
joao <desenvolvimento1@tricon.inf.br>
2023-07-12 16:52:37 -0300

    ssaasa

--- draw-app-masterV2/eraser.js
+++ draw-app-masterV2/eraser.js
@@ -3,16 +3,35 @@ function Eraser() {
     this.icon = "assets/eraser.jpg";
     this.name = "eraser";
 
-    this.draw = function () {
-        if (mouseIsPressed) {
-            fill(255);
-            noStroke();
-            ellipse(mouseX, mouseY, 20);
-        }
-    }
-
     this.unselectTool = function(){
         fill(colourP.selectedColour);
         stroke(colourP.selectedColour);
     }
+
+    this.draw = function(){
+		if(mouseIsPressed){
+			if (previousMouseX == -1){
+				previousMouseX = mouseX;
+				previousMouseY = mouseY;
+			}
+			else{
+                fill(255);
+                noStroke();
+                var n = 5;
+                var nx = (mouseX-previousMouseX)/n;
+                var ny = (mouseY-previousMouseY)/n;
+				while (Math.abs(previousMouseX-mouseX)>nx){
+                    ellipse(previousMouseX,previousMouseY,50)
+                    previousMouseX+=nx;
+                    previousMouseY+=ny;
+                }
+				previousMouseX = mouseX;
+				previousMouseY = mouseY;
+			}
+		}
+		else{
+			previousMouseX = -1;
+			previousMouseY = -1;
+		}
+	}
 }
