b500998f76b6d602164af35bbce274e86a449be3
joao <caneppele1.6@gmail.com>
2023-08-23 21:23:52 -0300

    testing change position after drawn

--- draw-app-masterV2/editableShapes/rectShape.js
+++ draw-app-masterV2/editableShapes/rectShape.js
@@ -2,11 +2,14 @@ function RectShape() {
     this.name = "rectShape";
     this.icon = "assets/shapes/rectShape.jpg";
     this.firstDrawing = true;
+    this.editingPosition = false;
     this.posX;
     this.posY;
+    var lastX;
+    var lastY;
 
     this.draw = function(previousX, previousY, x, y,fill){
-        if (previousX != -1) {
+        if (previousX != -1 && !this.editingPosition) {
             if (this.firstDrawing){
                 loadPixels();
                 this.posX = previousX;
@@ -14,11 +17,23 @@ function RectShape() {
                 this.firstDrawing = false;
             }
             updatePixels();
+            lastX = x;
+            lastY = y;
             rect(this.posX,this.posY,x-this.posX,y-this.posY);
+        }
+        else if(!this.firstDrawing){
+            this.editingPosition = true;
+            if (mouseIsPressed) this.firstDrawing = true;
+            var xdiff = x - lastX;
+            var ydiff = y - lastY;
+            var newX = this.posX+xdiff;
+            var newY = this.posY+ydiff;
+            updatePixels();
+            rect(newX,newY,x-newX,y-newY);
         }
         else {
             loadPixels();
-            this.firstDrawing = true;
+            this.editingPosition = false;
         }
     }
 }
